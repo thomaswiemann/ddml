574206600fb23cb3b54f0780757e4a2ee5bb233a
thomaswiemann <wiemann@uchicago.edu>
2021-01-21 17:13:39 +0100

    Added BLP 1995 example data
    Extensive example added to documentation. Uses BLP 1995 data. Inspired by CHS 2015. Other minor fixed to documentation included.

---
+++ data-raw/BLP_1995.R
@@ -0,0 +1,25 @@
+#' Data from Berry, Levinsohn, Pakes (1995)
+#'
+#' Data from Berry, Levinsohn, Pakes (1995) as retrived from the reproduction
+#' excercise in Chernozhukov, Hansen, and Spindler (2015)
+#'
+#'
+#' @format A data frame with 2217 rows and 17 variables:
+#' \describe{
+#'   \item{model.id}{ }
+#'   \item{id}{ }
+#'   ...
+#' }
+#' @source \url{/https://www.aeaweb.org/articles?id=10.1257/aer.p20151022/}
+
+library(R.matlab)
+dat <- df <- readMat("data-raw/BLP_data.mat")
+df$cdindex <- df$own.dummies <- NULL
+BLP_1995 <- data.frame(matrix(unlist(df), nrow=2217, byrow=F))
+colnames(BLP_1995) <- names(df)
+
+BLP_1995$model.name <- readMat("data-raw/BLP_data_str.mat")$model.name
+
+BLP_1995$own.dummies <- dat$own.dummies
+
+usethis::use_data(BLP_1995, overwrite = TRUE)
