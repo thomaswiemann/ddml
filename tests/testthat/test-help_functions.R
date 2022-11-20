1cf0d8f4e02a98933eb25b6a5bceb6741a0421ed
thomaswiemann <wiemann@uchicago.edu>
2022-11-20 13:57:36 -0600

    remove obsolete help_functions

--- tests/testthat/test-help_functions.R
+++ tests/testthat/test-help_functions.R
@@ -7,18 +7,3 @@ test_that("csolve returns a (generalized) inverse", {
   expect_equal(csolve(X) %*% X, diag(1, 30))
   expect_equal((csolve(X_sing) %*% X_sing) %*% X_sing, X_sing)
 })#TEST_THAT
-
-test_that("ccov returns a covariance matrix", {
-  # Simulate small matrices, one of them singular
-  X <- matrix(rnorm(6*100), 100, 6)
-  # Check combuted inverses
-  expect_equal(ccov(X), cov(X))
-})#TEST_THAT
-
-test_that("ccor returns a correlation matrix", {
-  # Simulate small matrices, one of them singular
-  X <- matrix(rnorm(6*100), 100, 6)
-  # Check combuted inverses
-  expect_equal(ccor(X), cor(X))
-  expect_equal(ccor(X, X[, 1:2]), cor(X, X[, 1:2]))
-})#TEST_THAT
