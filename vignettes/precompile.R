9754b12688eb0cc233232050dec8f7fa336ba0db
thomaswiemann <wiemann@uchicago.edu>
2023-08-25 12:59:11 -0500

    restructure vignettes & articles

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -2,13 +2,17 @@
 library(knitr)
 
 # depends on keras
-knit("vignettes/articles/new_ml_wrapper.Rmd.lol",
+knit("vignettes/articles/new_ml_wrapper.Rmd.txt",
      "vignettes/articles/new_ml_wrapper.Rmd")
 
 # depends on AER, hdm
-knit("vignettes/articles/example_BLP95.Rmd.lol",
+knit("vignettes/articles/example_BLP95.Rmd.txt",
      "vignettes/articles/example_BLP95.Rmd")
 
 # depends on Matrix
-knit("vignettes/articles/sparse.Rmd.lol",
+knit("vignettes/articles/example_401k.Rmd.txt",
+     "vignettes/articles/example_401k.Rmd")
+
+# depends on Matrix
+knit("vignettes/articles/sparse.Rmd.txt",
      "vignettes/articles/sparse.Rmd")
