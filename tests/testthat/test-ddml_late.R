4ddf59272388eaec6b08e20e69a9026afd4dc617
thomaswiemann <wiemann@uchicago.edu>
2023-11-04 11:19:52 -0500

    LATE w/ one-sided compliance

--- tests/testthat/test-ddml_late.R
+++ tests/testthat/test-ddml_late.R
@@ -18,7 +18,28 @@ test_that("ddml_late computes with a single model", {
   expect_equal(length(ddml_late_fit$late), 1)
 })#TEST_THAT
 
-test_that("ddml_plm computes with an ensemble procedure", {
+test_that("ddml_late computes with a single model & perfect non-compliance", {
+  # Simulate small dataset
+  nobs <- 200
+  X <- cbind(1, matrix(rnorm(nobs*39), nobs, 39))
+  Z_tld <-  X %*% runif(40) + rnorm(nobs)
+  Z <- 1 * (Z_tld > mean(Z_tld))
+  D_tld <-  0.5 * (1 - 2 * Z) + 0.2 * X %*% runif(40) + rnorm(nobs)
+  D <- 1 * (D_tld > mean(D_tld))
+  D[Z == 0] <- 0
+  y <- D + X %*% runif(40) + rnorm(nobs)
+  # Define arguments
+  learners <- list(what = ols)
+  ddml_late_fit <- ddml_late(y, D, Z, X,
+                             learners = learners,
+                             cv_folds = 3,
+                             sample_folds = 3,
+                             silent = T)
+  # Check output with expectations
+  expect_equal(length(ddml_late_fit$late), 1)
+})#TEST_THAT
+
+test_that("ddml_late computes with an ensemble procedure", {
   # Simulate small dataset
   nobs <- 200
   X <- cbind(1, matrix(rnorm(nobs*39), nobs, 39))
@@ -41,7 +62,7 @@ test_that("ddml_plm computes with an ensemble procedure", {
   expect_equal(length(ddml_late_fit$late), 1)
 })#TEST_THAT
 
-test_that("ddml_plm computes with multiple ensemble procedures", {
+test_that("ddml_late computes with multiple ensemble procedures", {
   # Simulate small dataset
   nobs <- 200
   X <- cbind(1, matrix(rnorm(nobs*39), nobs, 39))
@@ -66,7 +87,33 @@ test_that("ddml_plm computes with multiple ensemble procedures", {
   expect_equal(length(ddml_late_fit$late), 5)
 })#TEST_THAT
 
-test_that("ddml_plm computes with multiple ensemble procedures & shortstack", {
+test_that("ddml_late computes with multiple ensemble procedures + perfect compliance", {
+  # Simulate small dataset
+  nobs <- 200
+  X <- cbind(1, matrix(rnorm(nobs*39), nobs, 39))
+  Z_tld <-  X %*% runif(40) + rnorm(nobs)
+  Z <- 1 * (Z_tld > mean(Z_tld))
+  D_tld <-  0.25 * (1 - 2 * Z) + 0.2 * X %*% runif(40) + rnorm(nobs)
+  D <- 1 * (D_tld > mean(D_tld))
+  D[Z == 1] <- 1 # perfect compliance
+  y <- D + X %*% runif(40) + rnorm(nobs)
+  # Define arguments
+  learners <- list(list(fun = ols),
+                   list(fun = ols))
+  # Compute DDML PLM estimator
+  ddml_late_fit <- ddml_late(y, D, Z, X,
+                             learners,
+                             ensemble_type = c("ols", "nnls",
+                                               "nnls1",
+                                               "singlebest", "average"),
+                             cv_folds = 3,
+                             sample_folds = 3,
+                             silent = T)
+  # Check output with expectations
+  expect_equal(length(ddml_late_fit$late), 5)
+})#TEST_THAT
+
+test_that("ddml_late computes with multiple ensemble procedures & shortstack", {
   # Simulate small dataset
   nobs <- 200
   X <- cbind(1, matrix(rnorm(nobs*39), nobs, 39))
